2572f165acafd9acc417a24810ccb8e4021866c6
Rob Austin <rob.austin@boundedbuffer.co.uk>
2015-08-07 10:30:37 +0100

    fixed issue with test

--- src/main/java/net/openhft/chronicle/core/pool/ClassAliasPool.java
+++ src/main/java/net/openhft/chronicle/core/pool/ClassAliasPool.java
@@ -84,11 +84,15 @@ public class ClassAliasPool {
     }
 
     public String nameFor(Class clazz) {
+
         return classStringMap.computeIfAbsent(clazz, (aClass) -> {
             if (Enum.class.isAssignableFrom(aClass)) {
                 Class clazz2 = aClass.getEnclosingClass();
-                if (clazz2 != null)
+                if (clazz2 != null) {
                     aClass = clazz2;
+                    String alias = classStringMap.get(clazz2);
+                    if (alias != null) return alias;
+                }
             }
             return aClass.getName();
         });
