370230e63dc0233ffe525179d680c5b15af9eca5
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2015-08-11 17:16:09 +0100

    WIRE-33 Optimise bytes, wire for benchmark

---
+++ src/main/java/net/openhft/chronicle/core/pool/EnumInterner.java
@@ -0,0 +1,49 @@
+/*
+ *     Copyright (C) 2015  higherfrequencytrading.com
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.openhft.chronicle.core.pool;
+
+import net.openhft.chronicle.core.Maths;
+import net.openhft.chronicle.core.util.StringUtils;
+
+/**
+ * @author peter.lawrey
+ */
+public class EnumInterner<E extends Enum<E>> {
+    private final E[] interner;
+    private final int mask;
+    private final Class<E> eClass;
+
+    public EnumInterner(Class<E> eClass) {
+        this(eClass, 64);
+    }
+
+    public EnumInterner(Class<E> eClass, int capacity) {
+        this.eClass = eClass;
+        int n = Maths.nextPower2(capacity, 16);
+        interner = (E[]) new Enum[n];
+        mask = n - 1;
+    }
+
+    public E intern(CharSequence cs) {
+        int h = Maths.hash(cs) & mask;
+        E e = interner[h];
+        if (e != null && StringUtils.isEqual(e.name(), cs))
+            return e;
+        String s2 = cs.toString();
+        return interner[h] = Enum.valueOf(eClass, s2);
+    }
+}
