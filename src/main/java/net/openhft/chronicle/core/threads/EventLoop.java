f8158b00b1867707bb003fb9234aaee8203dd00d
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2016-02-11 16:47:53 -0500

    Move the api for thread into core to minimise direct dependencies on threads

---
+++ src/main/java/net/openhft/chronicle/core/threads/EventLoop.java
@@ -0,0 +1,37 @@
+/*
+ *     Copyright (C) 2015  higherfrequencytrading.com
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.openhft.chronicle.core.threads;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.Closeable;
+
+/**
+ * Created by peter.lawrey on 22/01/15.
+ */
+public interface EventLoop extends Closeable {
+
+    void addHandler(boolean dontAttemptToRunImmediatelyInCurrentThread, @NotNull EventHandler handler);
+
+    void addHandler(EventHandler handler);
+
+    void start();
+
+    void unpause();
+
+    void stop();
+}
