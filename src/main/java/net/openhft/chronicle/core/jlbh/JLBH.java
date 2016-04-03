7cf7ddd79977e4b02a4ee1313a312e1650312763
danielshaya <daniel@familyshaya.com>
2016-04-03 14:08:53 +0100

    JLBH summary print out 5 9s and 6 9s.

--- src/main/java/net/openhft/chronicle/core/jlbh/JLBH.java
+++ src/main/java/net/openhft/chronicle/core/jlbh/JLBH.java
@@ -208,12 +208,16 @@ public class JLBH implements NanoSampler {
         System.out.println(sb.toString());
 
         sb = new StringBuilder();
-        addPrToPrint(sb, "50:   ", jlbhOptions.runs);
-        addPrToPrint(sb, "90:   ", jlbhOptions.runs);
-        addPrToPrint(sb, "99:   ", jlbhOptions.runs);
-        addPrToPrint(sb, "99.9: ", jlbhOptions.runs);
-        addPrToPrint(sb, "99.99:", jlbhOptions.runs);
-        addPrToPrint(sb, "worst:", jlbhOptions.runs);
+        addPrToPrint(sb, "50:     ", jlbhOptions.runs);
+        addPrToPrint(sb, "90:     ", jlbhOptions.runs);
+        addPrToPrint(sb, "99:     ", jlbhOptions.runs);
+        addPrToPrint(sb, "99.9:   ", jlbhOptions.runs);
+        addPrToPrint(sb, "99.99:  ", jlbhOptions.runs);
+        if(jlbhOptions.iterations > 10_000_000)
+            addPrToPrint(sb, "99.999: ", jlbhOptions.runs);
+        if(jlbhOptions.iterations > 100_000_000)
+            addPrToPrint(sb, "99.9999:", jlbhOptions.runs);
+        addPrToPrint(sb, "worst:  ", jlbhOptions.runs);
 
         System.out.printf(sb.toString(), summary.toArray(NO_DOUBLES));
         System.out.println("-------------------------------------------------------------------------------------------------------------------");
