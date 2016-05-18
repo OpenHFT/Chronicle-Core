ba387d5bdf7633b9e52aedf689fff07b2ecd8a14
Rob Austin <rob.austin@boundedbuffer.co.uk>
2016-05-18 15:35:25 +0100

    changes added while testing queue replication at high load

--- src/main/java/net/openhft/chronicle/core/threads/InvalidEventHandlerException.java
+++ src/main/java/net/openhft/chronicle/core/threads/InvalidEventHandlerException.java
@@ -26,4 +26,8 @@ public class InvalidEventHandlerException extends Exception {
 
     public InvalidEventHandlerException() {
     }
+
+    public InvalidEventHandlerException(Throwable cause) {
+        super(cause);
+    }
 }
