d31ef7fac0a7b73a035403b3f6f4c7903445910e
Rob Austin <rob.austin@boundedbuffer.co.uk>
2016-08-11 22:09:02 +0100

    fixed reference count issue

--- src/main/java/net/openhft/chronicle/core/ReferenceCounter.java
+++ src/main/java/net/openhft/chronicle/core/ReferenceCounter.java
@@ -16,41 +16,78 @@
 
 package net.openhft.chronicle.core;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 public class ReferenceCounter {
     private final AtomicLong value = new AtomicLong(1);
     private final Runnable onRelease;
 
+    // records where is reference was created and released, only used for debuging and
+    // only active when assertions are turned on
+    private List<Exception> referenceCountHistory;
+
     private ReferenceCounter(Runnable onRelease) {
         this.onRelease = onRelease;
+        assert newRefCountHistory();
+    }
+
+
+    private boolean newRefCountHistory() {
+        referenceCountHistory = new ArrayList<>();
+        referenceCountHistory.add(new RuntimeException("creation ref-count=" + 0));
+        return true;
     }
 
     public static ReferenceCounter onReleased(Runnable onRelease) {
+
         return new ReferenceCounter(onRelease);
     }
 
     public void reserve() throws IllegalStateException {
         for (; ; ) {
+
             long v = value.get();
-            if (v <= 0)
+            assert recordResevation(v);
+            if (v <= 0) {
+                assert logReferenceCountHistory();
                 throw new IllegalStateException("Released");
+            }
             if (value.compareAndSet(v, v + 1))
                 break;
         }
     }
 
+    private boolean recordResevation(long v) {
+        referenceCountHistory.add(new RuntimeException("Reserve ref-count=" + v));
+        return true;
+    }
+
     public void release() throws IllegalStateException {
         for (; ; ) {
             long v = value.get();
-            if (v <= 0)
+            assert recordRelease(v);
+            if (v <= 0) {
+                assert logReferenceCountHistory();
                 throw new IllegalStateException("Released");
+            }
             if (value.compareAndSet(v, v - 1)) {
                 if (v == 1)
                     onRelease.run();
                 break;
             }
         }
+    }
+
+    private boolean logReferenceCountHistory() {
+        referenceCountHistory.forEach(Throwable::printStackTrace);
+        return true;
+    }
+
+    private boolean recordRelease(long v) {
+        referenceCountHistory.add(new RuntimeException("Release ref-count=" + v));
+        return true;
     }
 
     public long get() {
@@ -61,8 +98,4 @@ public class ReferenceCounter {
         return Long.toString(value.get());
     }
 
-    public void releaseAll() {
-        if (value.get() > 0)
-            onRelease.run();
-    }
 }
