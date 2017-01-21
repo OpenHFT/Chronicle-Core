428ee3511d48fad6b9d5a17dd5f7d8ec264e1299
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2017-01-21 11:00:08 +0000

    Support salting encrypted messages.

---
+++ src/main/java/net/openhft/chronicle/core/util/Updater.java
@@ -0,0 +1,15 @@
+package net.openhft.chronicle.core.util;
+
+import java.util.function.Consumer;
+
+/**
+ * This is similar to a Consumer but it is expected to alter the object whereas a Consumer is not expected to.
+ */
+@FunctionalInterface
+public interface Updater<T> extends Consumer<T> {
+    default void accept(T t) {
+        update(t);
+    }
+
+    void update(T t);
+}
