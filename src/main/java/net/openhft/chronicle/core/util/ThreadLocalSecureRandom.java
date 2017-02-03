3646b1042c796c0196af72fc2126457bd8ae8f18
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2017-02-03 17:10:46 +0000

    Improve Queue latency tests to consider the impact of encryption.

---
+++ src/main/java/net/openhft/chronicle/core/util/ThreadLocalSecureRandom.java
@@ -0,0 +1,17 @@
+package net.openhft.chronicle.core.util;
+
+import java.security.SecureRandom;
+
+/**
+ * Created by peter on 03/02/2017.
+ */
+public class ThreadLocalSecureRandom {
+    static final ThreadLocal<SecureRandom> SECURE_RANDOM_TL = new ThreadLocal<>();
+
+    public static SecureRandom current() {
+        SecureRandom sr = SECURE_RANDOM_TL.get();
+        if (sr == null)
+            SECURE_RANDOM_TL.set(sr = new SecureRandom());
+        return sr;
+    }
+}
