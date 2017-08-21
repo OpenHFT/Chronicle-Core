4f8bc174bcf8e969814c859a24452988492fc341
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2017-08-21 17:10:55 +0100

    Add benchmark for ThreadLocal

---
+++ benchmarks/src/main/java/net/openhft/chronicle/core/benchmark/Main.java
@@ -0,0 +1,82 @@
+/*
+ *     Copyright (C) 2015  higherfrequencytrading.com
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Lesser General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Lesser General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Lesser General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.openhft.chronicle.core.benchmark;
+
+import net.openhft.chronicle.core.Jvm;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.concurrent.TimeUnit;
+
+/*
+ * Created by Peter Lawrey on 11/08/15.
+ */
+@State(Scope.Thread)
+public class Main {
+
+    ThreadLocal threadLocal = ThreadLocal.withInitial(Object::new);
+
+    public static void main(String... args) throws RunnerException, InvocationTargetException, IllegalAccessException {
+        if (Jvm.isDebug()) {
+            Main main = new Main();
+            for (Method m : Main.class.getMethods()) {
+                if (m.getAnnotation(Benchmark.class) != null) {
+                    m.invoke(main);
+                }
+            }
+        } else {
+            int time = Boolean.getBoolean("longTest") ? 30 : 2;
+            System.out.println("measurementTime: " + time + " secs");
+            Options opt = new OptionsBuilder()
+                    .include(Main.class.getSimpleName())
+//                    .warmupIterations(5)
+                    .measurementIterations(5)
+                    .forks(10)
+                    .mode(Mode.SampleTime)
+                    .measurementTime(TimeValue.seconds(time))
+                    .timeUnit(TimeUnit.NANOSECONDS)
+                    .build();
+
+            new Runner(opt).run();
+        }
+    }
+
+    //    @Benchmark
+    public Thread currentThread() {
+        return Thread.currentThread();
+    }
+
+    //    @Benchmark
+    public long currentThread_getId() {
+        return Thread.currentThread().getId();
+    }
+
+    @Benchmark
+    public Object threadLocal_get() {
+        return threadLocal.get();
+    }
+}
+
