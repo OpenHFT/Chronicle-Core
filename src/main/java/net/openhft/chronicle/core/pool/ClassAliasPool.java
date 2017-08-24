a5590fae2732314ce12be55db81d26d0b213ac28
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2017-08-24 14:34:57 +0100

    When a class alias is missing, don't set it to the full class name, just leave it as a default.

--- src/main/java/net/openhft/chronicle/core/pool/ClassAliasPool.java
+++ src/main/java/net/openhft/chronicle/core/pool/ClassAliasPool.java
@@ -142,17 +142,16 @@ public class ClassAliasPool implements ClassLookup {
     }
 
     private String nameFor0(Class clazz) {
-        return classStringMap.computeIfAbsent(clazz, (aClass) -> {
-            if (Enum.class.isAssignableFrom(aClass)) {
-                Class clazz2 = aClass.getSuperclass();
-                if (clazz2 != null && clazz2 != Enum.class && Enum.class.isAssignableFrom(clazz2)) {
-                    aClass = clazz2;
-                    String alias = classStringMap.get(clazz2);
-                    if (alias != null) return alias;
-                }
+        if (Enum.class.isAssignableFrom(clazz)) {
+            Class clazz2 = clazz.getSuperclass();
+            if (clazz2 != null && clazz2 != Enum.class && Enum.class.isAssignableFrom(clazz2)) {
+                String alias = classStringMap.get(clazz2);
+                return alias != null
+                        ? classStringMap.putIfAbsent(clazz, alias)
+                        : clazz2.getName();
             }
-            return aClass.getName();
-        });
+        }
+        return clazz.getName();
     }
 
     @Override
