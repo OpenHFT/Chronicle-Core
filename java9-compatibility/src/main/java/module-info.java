1773d7f23143d91ec559fdb395ecfb0598ef0c5a
epickrram <mark@epickrram.com>
2017-09-04 14:05:03 +0100

    remove test modules before merge

--- java9-compatibility/src/main/java/module-info.java
+++
@@ -1,5 +0,0 @@
-module net.openhft.chronicle.core {
-    requires chronicle.core;
-    provides net.openhft.chronicle.core.cleaner.spi.ByteBufferCleanerService
-            with net.openhft.chronicle.core.cleaner.impl.jdk9.Jdk9ByteBufferCleanerService;
-}
--- java9-module-test/src/main/java/module-info.java
+++
@@ -1,4 +0,0 @@
-module java_nine.module.test {
-    requires chronicle.core;
-    requires net.openhft.chronicle.core;
-}
