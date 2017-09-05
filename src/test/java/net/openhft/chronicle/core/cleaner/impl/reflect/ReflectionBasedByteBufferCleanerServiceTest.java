0953338328ccd6fa0ff218caf5507f2e0aafd1b8
epickrram <mark@epickrram.com>
2017-09-05 13:42:00 +0100

    run test under java9

--- src/test/java/net/openhft/chronicle/core/cleaner/impl/reflect/ReflectionBasedByteBufferCleanerServiceTest.java
+++ src/test/java/net/openhft/chronicle/core/cleaner/impl/reflect/ReflectionBasedByteBufferCleanerServiceTest.java
@@ -1,16 +1,12 @@
 package net.openhft.chronicle.core.cleaner.impl.reflect;
 
-import net.openhft.chronicle.core.Jvm;
 import org.junit.Test;
 
 import java.nio.ByteBuffer;
 
-import static org.junit.Assume.assumeFalse;
-
 public class ReflectionBasedByteBufferCleanerServiceTest {
     @Test
     public void shouldCleanBuffer() throws Exception {
-        assumeFalse(Jvm.isJava9Plus());
         new ReflectionBasedByteBufferCleanerService().clean(ByteBuffer.allocateDirect(64));
     }
 }
