19e404fec9a4e8370e77aa3b2a8e41f8e1b4dc2d
Rob Austin <rob.austin@boundedbuffer.co.uk>
2018-08-24 15:30:32 +0100

    https://github.com/ChronicleEnterprise/Chronicle-Queue-Enterprise/issues/56 first cut, introduced "boolean offer(@NotNull Consumer<Bytes> consumer, long length);" with test cases, still requires full integration into the rest of the stack.

---
+++ src/main/java/net/openhft/chronicle/core/util/ThrowingTriFunction.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2016 higherfrequencytrading.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.openhft.chronicle.core.util;
+
+import org.jetbrains.annotations.NotNull;
+
+@FunctionalInterface
+public interface ThrowingTriFunction<I, J, A, R, T extends Throwable> {
+
+    /**
+     * Applies this function to the given arguments.
+     *
+     * @param in the first function argument
+     * @param i2 the second function argument
+     * @return the function result
+     * @throws T on an error.
+     */
+    @NotNull
+    R apply(I in, J i2, A i3) throws T;
+}
