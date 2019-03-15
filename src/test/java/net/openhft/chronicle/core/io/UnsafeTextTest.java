e5c437f44b7ea6e76bb994d08c828e6cec7b09ee
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2019-03-15 09:05:46 +0000

    @Ignore a performance test

--- src/test/java/net/openhft/chronicle/core/io/UnsafeTextTest.java
+++ src/test/java/net/openhft/chronicle/core/io/UnsafeTextTest.java
@@ -3,14 +3,17 @@ package net.openhft.chronicle.core.io;
 import net.openhft.chronicle.core.cooler.CoolerTester;
 import net.openhft.chronicle.core.cooler.CpuCooler;
 import net.openhft.chronicle.core.cooler.CpuCoolers;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static net.openhft.chronicle.core.UnsafeMemory.UNSAFE;
 
 public class UnsafeTextTest {
 
-static long blackhole;
+    static long blackhole;
+
     @Test
+    @Ignore("Performance test")
     public void coolerAppendBase10() {
         long address = UNSAFE.allocateMemory(32);
         new CoolerTester(new CpuCooler[]{
@@ -18,16 +21,8 @@ static long blackhole;
                 CpuCoolers.BUSY100
         })
 //                .add("noop", () -> null)
-                .add("1d", () -> {
-                    blackhole = UnsafeText.appendBase10(address, 1);
-                    return null;
-                })
-                .add("2d", () -> {
-                    blackhole = UnsafeText.appendBase10(address, 11);
-                    return null;
-                })
-                .add("9d", () -> {
-                    blackhole = UnsafeText.appendBase10(address, Integer.MAX_VALUE);
+                .add("20d", () -> {
+                    blackhole = UnsafeText.appendBase10(address, -Integer.MAX_VALUE);
                     return null;
                 })
                 .runTimeMS(10000)
