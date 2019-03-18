1514305f2ff59b151282f54c1f07b5d4ee483715
Rob Austin <rob.austin@boundedbuffer.co.uk>
2019-03-18 17:51:51 +0000

    fixed issue with ccy pairs

--- src/main/java/net/openhft/chronicle/core/util/StringUtils.java
+++ src/main/java/net/openhft/chronicle/core/util/StringUtils.java
@@ -428,4 +428,74 @@ public enum StringUtils {
             text.setCharAt(end - i, ch);
         }
     }
+
+    public static int parseInt(CharSequence s, int radix)
+            throws NumberFormatException {
+        /*
+         * WARNING: This method may be invoked early during VM initialization
+         * before IntegerCache is initialized. Care must be taken to not use
+         * the valueOf method.
+         */
+
+        if (s == null) {
+            throw new NumberFormatException("null");
+        }
+
+        if (radix < Character.MIN_RADIX) {
+            throw new NumberFormatException("radix " + radix +
+                    " less than Character.MIN_RADIX");
+        }
+
+        if (radix > Character.MAX_RADIX) {
+            throw new NumberFormatException("radix " + radix +
+                    " greater than Character.MAX_RADIX");
+        }
+
+        int result = 0;
+        boolean negative = false;
+        int i = 0, len = s.length();
+        int limit = -Integer.MAX_VALUE;
+        int multmin;
+        int digit;
+
+        if (len > 0) {
+            char firstChar = s.charAt(0);
+            if (firstChar < '0') { // Possible leading "+" or "-"
+                if (firstChar == '-') {
+                    negative = true;
+                    limit = Integer.MIN_VALUE;
+                } else if (firstChar != '+')
+                    throw forInputString(s);
+
+                if (len == 1) // Cannot have lone "+" or "-"
+                    throw forInputString(s);
+                i++;
+            }
+            multmin = limit / radix;
+            while (i < len) {
+                // Accumulating negatively avoids surprises near MAX_VALUE
+                digit = Character.digit(s.charAt(i++), radix);
+                if (digit < 0) {
+                    throw forInputString(s);
+                }
+                if (result < multmin) {
+                    throw forInputString(s);
+                }
+                result *= radix;
+                if (result < limit + digit) {
+                    throw forInputString(s);
+                }
+                result -= digit;
+            }
+        } else {
+            throw forInputString(s);
+        }
+        return negative ? result : -result;
+    }
+
+    static NumberFormatException forInputString(CharSequence s) {
+        return new NumberFormatException("For input string: \"" + s + "\"");
+    }
+
+
 }
