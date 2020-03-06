61767619b9a42b75b5e646f1e807fe91e1e3a5cb
Rob Austin <rob.austin@boundedbuffer.co.uk>
2020-03-06 10:12:00 +0000

    remvoved white space

--- src/main/java/net/openhft/chronicle/core/LicenceCheck.java
+++ src/main/java/net/openhft/chronicle/core/LicenceCheck.java
@@ -29,7 +29,6 @@ import static net.openhft.chronicle.core.Jvm.warn;
 public interface LicenceCheck {
 
     String CHRONICLE_LICENSE = "chronicle.license";
-
     static void check(String product, Class caller) {
         Jvm.debug(); // make sure this was loaded first.
         String key = System.getProperty(CHRONICLE_LICENSE);
