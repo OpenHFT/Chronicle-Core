3e6eca161f948a8f2e3f022b7c9a5193a21dd74c
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2020-05-07 10:30:32 +0100

    GC'ing a RandomAccessFile without closing it results in a resource leak

---
+++ src/test/java/net/openhft/chronicle/core/RandomAccessFileCleanupMain.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2016-2019 Chronicle Software Ltd
+ */
+
+package net.openhft.chronicle.core;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+
+/*
+NOTE: RandomAccessFile doesn't clean up it's resources when GC'ed
+ */
+public class RandomAccessFileCleanupMain {
+    public static void main(String[] args) throws IOException {
+        File tempDir = new File(OS.TMP, "raf-" + System.nanoTime());
+        tempDir.mkdir();
+        for (int j = 0; j < 100; j++) {
+            int files = new File("/proc/self/fd").list().length;
+            System.out.println("File descriptors " + files);
+            ByteBuffer bb = ByteBuffer.allocateDirect(64);
+            for (int i = 0; i < 100; i++) {
+                RandomAccessFile file = new RandomAccessFile(tempDir + "/file" + i, "rw");
+                bb.clear();
+                file.getChannel().write(bb);
+            }
+            System.gc();
+        }
+    }
+}
