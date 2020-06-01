2d4b8ed5288699954c562439812b7495b42334f2
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2020-06-01 14:01:22 +0100

    Support an OnDemandEventLoop for the SCQ builder

---
+++ src/test/java/net/openhft/chronicle/core/threads/OnDemandEventLoopTest.java
@@ -0,0 +1,61 @@
+package net.openhft.chronicle.core.threads;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OnDemandEventLoopTest {
+    @Test
+    public void onDemand() {
+        OnDemandEventLoop el = new OnDemandEventLoop(() -> new EventLoop() {
+            @Override
+            public String name() {
+                return "dummy";
+            }
+
+            @Override
+            public void addHandler(EventHandler handler) {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public void start() {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public void unpause() {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public void stop() {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public boolean isClosed() {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public boolean isAlive() {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public void awaitTermination() {
+                throw new UnsupportedOperationException();
+            }
+
+            @Override
+            public void close() {
+            }
+        });
+        assertFalse(el.hasEventLoop());
+        assertEquals("dummy", el.name());
+        assertTrue(el.hasEventLoop());
+        el.close();
+    }
+
+}
