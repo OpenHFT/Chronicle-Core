b7d609ea80ace25a7bfe5e49e6db42c0e09bb837
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2020-06-04 09:58:24 +0100

    Added a FlakyTestRunner, https://github.com/OpenHFT/Chronicle-Core/issues/120

--- src/main/java/net/openhft/chronicle/core/FlakyTestRunner.java
+++ src/main/java/net/openhft/chronicle/core/FlakyTestRunner.java
@@ -5,14 +5,24 @@ import net.openhft.chronicle.core.onoes.Slf4jExceptionHandler;
 public enum FlakyTestRunner {
     ;
 
+    static boolean IN_RUN = false;
     public static <T extends Throwable> void run(RunnableThrows<T> rt) throws T {
         try {
+            // stop accidental call back to self.
+            if (IN_RUN)
+                throw new AssertionError("Can't run nested");
+            IN_RUN = true;
+
             rt.run();
+
         } catch (Throwable t) {
+            System.err.println("Rerunning failing test");
             System.gc();
             Jvm.pause(500);
             rt.run();
             Slf4jExceptionHandler.WARN.on(FlakyTestRunner.class, "Flaky test threw an error the first run, passed the second time", t);
+        } finally {
+            IN_RUN = false;
         }
     }
 
