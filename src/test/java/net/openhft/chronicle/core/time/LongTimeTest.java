ef8433a92795f7981a1fab7c1674e60b934b53e9
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2020-08-20 13:12:39 +0100

    Improved consistency of support for Seconds, Millis, Micros and Nanos, https://github.com/OpenHFT/Chronicle-Core/issues/133

--- src/test/java/net/openhft/chronicle/core/time/LongTimeTest.java
+++ src/test/java/net/openhft/chronicle/core/time/LongTimeTest.java
@@ -7,7 +7,7 @@ import static org.junit.Assert.*;
 public class LongTimeTest {
     @Test
     public void secs() {
-        long epoch = LongTime.EPOCH_MILLIS;
+        long epoch = LongTime.EPOCH_SECS;
         assertTrue(LongTime.isSecs(epoch));
         assertTrue(LongTime.isSecs(LongTime.MAX_SECS));
         assertFalse(LongTime.isSecs(LongTime.EPOCH_MILLIS));
@@ -26,7 +26,7 @@ public class LongTimeTest {
         assertFalse(LongTime.isMillis(LongTime.EPOCH_MICROS));
         assertTrue(LongTime.isMillis(LongTime.MAX_SECS * 1000));
         assertTrue(LongTime.isMillis(LongTime.EPOCH_MICROS / 1000));
-        assertEquals(epoch, LongTime.toMillis(LongTime.toSecs(epoch)));
+        assertEquals(epoch - epoch % 1000, LongTime.toMillis(LongTime.toSecs(epoch)));
         assertEquals(epoch, LongTime.toMillis(LongTime.toMillis(epoch)));
         assertEquals(epoch, LongTime.toMillis(LongTime.toMicros(epoch)));
         assertEquals(epoch, LongTime.toMillis(LongTime.toNanos(epoch)));
@@ -40,21 +40,21 @@ public class LongTimeTest {
         assertFalse(LongTime.isMicros(LongTime.EPOCH_NANOS));
         assertTrue(LongTime.isMicros(LongTime.MAX_MILLIS * 1000));
         assertTrue(LongTime.isMicros(LongTime.EPOCH_NANOS / 1000));
-        assertEquals(epoch, LongTime.toMicros(LongTime.toSecs(epoch)));
-        assertEquals(epoch, LongTime.toMicros(LongTime.toMillis(epoch)));
+        assertEquals(epoch - epoch % 1000000, LongTime.toMicros(LongTime.toSecs(epoch)));
+        assertEquals(epoch - epoch % 1000, LongTime.toMicros(LongTime.toMillis(epoch)));
         assertEquals(epoch, LongTime.toMicros(LongTime.toMicros(epoch)));
         assertEquals(epoch, LongTime.toMicros(LongTime.toNanos(epoch)));
     }
 
     @Test
     public void nanos() {
-        long epoch = LongTime.EPOCH_MICROS;
+        long epoch = LongTime.EPOCH_NANOS;
         assertTrue(LongTime.isNanos(epoch));
         assertTrue(LongTime.isNanos(LongTime.MAX_NANOS));
         assertTrue(LongTime.isNanos(LongTime.MAX_MICROS * 1000));
-        assertEquals(epoch, LongTime.toNanos(LongTime.toSecs(epoch)));
-        assertEquals(epoch, LongTime.toNanos(LongTime.toMillis(epoch)));
-        assertEquals(epoch, LongTime.toNanos(LongTime.toMicros(epoch)));
+        assertEquals(epoch - epoch % 1000000000, LongTime.toNanos(LongTime.toSecs(epoch)));
+        assertEquals(epoch - epoch % 1000000, LongTime.toNanos(LongTime.toMillis(epoch)));
+        assertEquals(epoch - epoch % 1000, LongTime.toNanos(LongTime.toMicros(epoch)));
         assertEquals(epoch, LongTime.toNanos(LongTime.toNanos(epoch)));
     }
 }
