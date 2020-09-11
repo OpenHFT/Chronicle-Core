706d32bd122b9765f6286a48b91a9090616423bc
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2020-09-11 10:16:27 +0100

    Added support for ThreadLocal system properties, closes https://github.com/OpenHFT/Chronicle-Core/issues/137

---
+++ src/main/java/net/openhft/chronicle/core/threads/ThreadLocalProperties.java
@@ -0,0 +1,163 @@
+package net.openhft.chronicle.core.threads;
+
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class ThreadLocalProperties extends Properties {
+    final ThreadLocal<Properties> tl = ThreadLocal.withInitial(() -> new Properties(defaults));
+
+    public ThreadLocalProperties(Properties defaults) {
+        super(defaults);
+    }
+
+    public static void forSystemProperties() {
+        synchronized (System.class) {
+            Properties properties = System.getProperties();
+            if (properties instanceof ThreadLocalProperties)
+                return;
+            System.setProperties(new ThreadLocalProperties(properties));
+        }
+    }
+
+    @Override
+    public Object setProperty(String key, String value) {
+        return tl.get().setProperty(key, value);
+    }
+
+    @Override
+    public String getProperty(String key) {
+        return tl.get().getProperty(key);
+    }
+
+    @Override
+    public String getProperty(String key, String defaultValue) {
+        return tl.get().getProperty(key, defaultValue);
+    }
+
+    @Override
+    public Enumeration<?> propertyNames() {
+        return tl.get().propertyNames();
+    }
+
+    @Override
+    public Set<String> stringPropertyNames() {
+        return tl.get().stringPropertyNames();
+    }
+
+    @Override
+    public Object get(Object key) {
+        return tl.get().get(key);
+    }
+
+    @Override
+    protected void rehash() {
+        // no implemented
+    }
+
+    @Override
+    public Object put(Object key, Object value) {
+        return tl.get().put(key, value);
+    }
+
+    @Override
+    public Object remove(Object key) {
+        return tl.get().remove(key);
+    }
+
+    @Override
+    public void clear() {
+        tl.get().clear();
+    }
+
+    @Override
+    public Object clone() {
+        return tl.get().clone();
+    }
+
+    @Override
+    public String toString() {
+        return tl.get().toString();
+    }
+
+    @Override
+    public Set<Object> keySet() {
+        return tl.get().keySet();
+    }
+
+    @Override
+    public Set<Map.Entry<Object, Object>> entrySet() {
+        return tl.get().entrySet();
+    }
+
+    @Override
+    public Collection<Object> values() {
+        return tl.get().values();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return tl.get().equals(o);
+    }
+
+    @Override
+    public int hashCode() {
+        return tl.get().hashCode();
+    }
+
+    @Override
+    public Object getOrDefault(Object key, Object defaultValue) {
+        return tl.get().getOrDefault(key, defaultValue);
+    }
+
+    @Override
+    public void forEach(BiConsumer<? super Object, ? super Object> action) {
+        tl.get().forEach(action);
+    }
+
+    @Override
+    public void replaceAll(BiFunction<? super Object, ? super Object, ?> function) {
+        tl.get().replaceAll(function);
+    }
+
+    @Override
+    public Object putIfAbsent(Object key, Object value) {
+        return tl.get().putIfAbsent(key, value);
+    }
+
+    @Override
+    public boolean remove(Object key, Object value) {
+        return tl.get().remove(key, value);
+    }
+
+    @Override
+    public boolean replace(Object key, Object oldValue, Object newValue) {
+        return tl.get().replace(key, oldValue, newValue);
+    }
+
+    @Override
+    public Object replace(Object key, Object value) {
+        return tl.get().replace(key, value);
+    }
+
+    @Override
+    public Object computeIfAbsent(Object key, Function<? super Object, ?> mappingFunction) {
+        return tl.get().computeIfAbsent(key, mappingFunction);
+    }
+
+    @Override
+    public Object computeIfPresent(Object key, BiFunction<? super Object, ? super Object, ?> remappingFunction) {
+        return tl.get().computeIfPresent(key, remappingFunction);
+    }
+
+    @Override
+    public Object compute(Object key, BiFunction<? super Object, ? super Object, ?> remappingFunction) {
+        return tl.get().compute(key, remappingFunction);
+    }
+
+    @Override
+    public Object merge(Object key, Object value, BiFunction<? super Object, ? super Object, ?> remappingFunction) {
+        return tl.get().merge(key, value, remappingFunction);
+    }
+}
---
+++ src/test/java/net/openhft/chronicle/core/threads/ThreadLocalPropertiesTest.java
@@ -0,0 +1,54 @@
+package net.openhft.chronicle.core.threads;
+
+import net.openhft.chronicle.core.CoreTestCommon;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.junit.Assert.assertEquals;
+
+public class ThreadLocalPropertiesTest extends CoreTestCommon {
+
+    private Properties properties;
+
+    @Before
+    public void save() {
+        properties = System.getProperties();
+    }
+
+    @After
+    public void restore() {
+        System.setProperties(properties);
+    }
+
+    @Test
+    public void forSystemProperties() throws ExecutionException, InterruptedException {
+        Properties properties = new Properties(System.getProperties());
+        properties.setProperty("a", "none");
+        properties.setProperty("b", "none");
+        System.setProperties(properties);
+
+        ThreadLocalProperties.forSystemProperties();
+        ExecutorService es1 = Executors.newSingleThreadExecutor();
+        ExecutorService es2 = Executors.newSingleThreadExecutor();
+        ExecutorService es3 = Executors.newSingleThreadExecutor();
+        es1.submit(() -> System.setProperty("a", "one")).get();
+        es2.submit(() -> System.setProperty("b", "two")).get();
+        es3.submit(() -> System.setProperty("a", "three")).get();
+        es3.submit(() -> System.setProperty("b", "three")).get();
+        es1.submit(() -> assertEquals("one", System.getProperty("a"))).get();
+        es1.submit(() -> assertEquals("none", System.getProperty("b"))).get();
+        es2.submit(() -> assertEquals("none", System.getProperty("a"))).get();
+        es2.submit(() -> assertEquals("two", System.getProperty("b"))).get();
+        es3.submit(() -> assertEquals("three", System.getProperty("a"))).get();
+        es3.submit(() -> assertEquals("three", System.getProperty("b"))).get();
+        es1.shutdownNow();
+        es2.shutdownNow();
+        es3.shutdownNow();
+    }
+}
