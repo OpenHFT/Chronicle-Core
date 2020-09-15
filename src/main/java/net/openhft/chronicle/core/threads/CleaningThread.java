a4fdc4acdb642732f0e2b541e29f4ebab726d1ef
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2020-09-15 17:46:00 +0100

    Shutdown a replication test more cleanly.

--- src/main/java/net/openhft/chronicle/core/threads/CleaningThread.java
+++ src/main/java/net/openhft/chronicle/core/threads/CleaningThread.java
@@ -3,11 +3,22 @@ package net.openhft.chronicle.core.threads;
 import net.openhft.chronicle.core.Jvm;
 
 import java.lang.ref.WeakReference;
+import java.lang.reflect.Field;
 
 /**
  * This will clean up any {@link CleaningThreadLocal}
  */
 public class CleaningThread extends Thread {
+    private static final Field THREAD_LOCALS;
+    private static final Field TABLE;
+    private static final Field VALUE;
+
+    static {
+        THREAD_LOCALS = Jvm.getField(Thread.class, "threadLocals");
+        TABLE = Jvm.getField(THREAD_LOCALS.getType(), "table");
+        VALUE = Jvm.getField(TABLE.getType().getComponentType(), "value");
+    }
+
     public CleaningThread(Runnable target) {
         super(target);
     }
@@ -17,17 +28,33 @@ public class CleaningThread extends Thread {
     }
 
     public static void performCleanup(Thread thread) {
-        WeakReference[] table = Jvm.getValue(thread, "threadLocals/table");
-        if (table != null) {
-            for (WeakReference reference : table) {
-                Object key = reference != null ? reference.get() : null;
-                if (key instanceof CleaningThreadLocal) {
-                    Object value = Jvm.getValue(reference, "value");
-                    if (value != null) {
-                        CleaningThreadLocal ctl = (CleaningThreadLocal) key;
-                        ctl.cleanup(value);
-                    }
-                }
+        WeakReference[] table;
+        try {
+            Object o = THREAD_LOCALS.get(thread);
+            if (o == null)
+                return;
+            table = (WeakReference[]) TABLE.get(o);
+        } catch (IllegalAccessException e) {
+            Jvm.debug().on(CleaningThreadLocal.class, e.toString());
+            return;
+        }
+        if (table == null)
+            return;
+
+        for (WeakReference reference : table) {
+            Object key = reference != null ? reference.get() : null;
+            if (!(key instanceof CleaningThreadLocal))
+                continue;
+
+            try {
+                Object value = VALUE.get(reference);
+                if (value == null)
+                    continue;
+
+                CleaningThreadLocal ctl = (CleaningThreadLocal) key;
+                ctl.cleanup(value);
+            } catch (IllegalAccessException e) {
+                Jvm.debug().on(CleaningThreadLocal.class, e.toString());
             }
         }
     }
