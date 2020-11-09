2e13733cdb0889533c6d738216b45f08b4264102
Per Minborg <minborg@speedment.com>
2020-11-09 20:11:30 +0100

    Change test

--- src/test/java/net/openhft/chronicle/core/analytics/AnalyticsTest.java
+++ src/test/java/net/openhft/chronicle/core/analytics/AnalyticsTest.java
@@ -1,5 +1,6 @@
 package net.openhft.chronicle.core.analytics;
 
+import net.openhft.chronicle.core.Jvm;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -11,8 +12,11 @@ public class AnalyticsTest {
         final Analytics analytics = Analytics.acquire("map", "3.20.84-SNAPSHOT");
         analytics.onStart();
 
-        System.out.println("Press enter to stop");
-        System.in.read();
+/*        System.out.println("Press enter to stop");
+        System.in.read();*/
+
+        // Give the http thread time to complete
+        Jvm.pause(2000);
 
     }
 
