4f7b5c1679a633d13a87a7c8aa27e94cacec7d06
Per Minborg <minborg@speedment.com>
2020-11-13 13:12:38 +0100

    Add test for #151

---
+++ src/test/java/net/openhft/chronicle/core/NotNullIntrumentationTargetTest.java
@@ -0,0 +1,24 @@
+package net.openhft.chronicle.core;
+
+import org.jetbrains.annotations.NotNull;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class NotNullIntrumentationTargetTest {
+
+    @Test
+    public void notNull() {
+        test("a");
+    }
+
+    @Test(expected = NullPointerException.class)
+    @Ignore // Awaiting https://github.com/osundblad/intellij-annotations-instrumenter-maven-plugin/issues/53
+    public void Null() {
+        test(null);
+    }
+
+    private static void test(@NotNull String nn) {
+        // This should throw an NPE if called with a null argument
+    }
+
+}
