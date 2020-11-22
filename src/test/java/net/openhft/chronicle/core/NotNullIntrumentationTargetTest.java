ad869387f43d136ad80d2d86cd213a79741f7699
Jerry Shea <jerry.shea@chronicle.software>
2020-11-22 12:41:15 +1100

    Doco https://github.com/OpenHFT/Chronicle-Core/pull/154

--- src/test/java/net/openhft/chronicle/core/NotNullIntrumentationTargetTest.java
+++ src/test/java/net/openhft/chronicle/core/NotNullIntrumentationTargetTest.java
@@ -13,7 +13,7 @@ public class NotNullIntrumentationTargetTest {
 
     @Test(expected = NullPointerException.class)
     @Ignore("Awaiting https://github.com/osundblad/intellij-annotations-instrumenter-maven-plugin/issues/53. " +
-            "This behaves as expected when run in IntelliJ as IntelliJ compiler instruments classes with null checks")
+            "When compiled by IntelliJ this class is instrumented with null checks but it will throw an IllegalArgumentExceptioj not NPE!")
     public void Null() {
         test(null);
     }
