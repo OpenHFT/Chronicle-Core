ab159e07981bc04196eb1c1aee9af63edeb07047
Rob Austin <rob.austin@boundedbuffer.co.uk>
2020-12-30 15:44:03 +0000

    removed deprecated

--- src/main/java/net/openhft/chronicle/core/ClassLocal.java
+++ src/main/java/net/openhft/chronicle/core/ClassLocal.java
@@ -48,11 +48,8 @@ public class ClassLocal<V> extends ClassValue<V> {
 
     /**
      * WARNING Do not call this directly
-     *
-     * @deprecated do not call this directly
      */
     @Override
-    @Deprecated(/* to be removed in x.22 */)
     protected V computeValue(Class<?> type) {
         return classVFunction.apply(type);
     }
