601c24bc73ea349e1cda9ba14c4b999a8cb75b8b
Per Minborg <minborg@speedment.com>
2021-01-07 19:33:43 +0100

    Improve Builder doc and deprecate get(), work on #185

--- src/main/java/net/openhft/chronicle/core/util/Builder.java
+++ src/main/java/net/openhft/chronicle/core/util/Builder.java
@@ -5,16 +5,36 @@ import org.jetbrains.annotations.NotNull;
 import java.util.function.Supplier;
 
 /**
- * A Builder is a configurable Supplier which always creates a new object.
+ * A Builder of type T is a configurable object that can provide
+ * another non-null instance of type T.
  *
- * @param <T> Of object created
+ * @param <T> of object provided
  */
+@FunctionalInterface
 public interface Builder<T> extends Supplier<T> {
+
+    /**
+     * Builds and returns a non-null instance of type T.
+     * <p>
+     * The builder always creates a new instance if the
+     * instance is mutable. If the instance is immutable,
+     * the builder may create a new instance or it may return
+     * a previously existing instance at its own discretion.
+     * <p>
+     * As opposed to a factory, a Builder is often unipotent
+     * meaning that this method can be invoked at most one time.
+     *
+     * @return a non-null instance of type T
+     *
+     * @throws IllegalStateException if the builder is unipotent
+     * and this method is invoked more than once.
+     */
     @NotNull
     T build();
 
     @Override
     @NotNull
+    @Deprecated // For removal now
     default T get() {
         return build();
     }
