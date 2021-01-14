3fadc3c4f45c3ab4189f1ae9b3cd028bed594d4a
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2021-01-14 14:22:36 +0000

    Improve Azul Zing support, https://github.com/OpenHFT/OpenHFT/issues/47

--- src/test/java/net/openhft/chronicle/core/io/BackgroundResourceReleaserTest.java
+++ src/test/java/net/openhft/chronicle/core/io/BackgroundResourceReleaserTest.java
@@ -43,7 +43,7 @@ public class BackgroundResourceReleaserTest {
         long start = System.currentTimeMillis();
         BackgroundResourceReleaser.releasePendingResources();
         long time = System.currentTimeMillis() - start;
-        assertBetween(count * 4, time, count * 11);
+        assertBetween(count * 4, time, count * 13);
         assertEquals(count, closed.get());
         assertEquals(count, released.get());
         AbstractCloseable.assertCloseablesClosed();
