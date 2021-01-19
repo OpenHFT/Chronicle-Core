a93b2a38cf6b4f85a7d6a135cbd490b8b8bb30e1
Jerry Shea <jerry.shea@chronicle.software>
2021-01-19 11:32:22 +1100

    test to ensure UniqueMicroTimeProvider provides output in correct units

--- src/test/java/net/openhft/chronicle/core/time/UniqueMicroTimeProviderTest.java
+++ src/test/java/net/openhft/chronicle/core/time/UniqueMicroTimeProviderTest.java
@@ -2,6 +2,7 @@ package net.openhft.chronicle.core.time;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class UniqueMicroTimeProviderTest {
@@ -9,12 +10,13 @@ public class UniqueMicroTimeProviderTest {
     @Test
     public void currentTimeMicros() {
         UniqueMicroTimeProvider tp = new UniqueMicroTimeProvider();
-        SetTimeProvider stp = new SetTimeProvider();
+        SetTimeProvider stp = new SetTimeProvider(SystemTimeProvider.INSTANCE.currentTimeNanos());
         tp.provider(stp);
         long last = 0;
         for (int i = 0; i < 4_000; i++) {
             stp.advanceNanos(i);
             long time = tp.currentTimeMicros();
+            assertEquals(LongTime.toMicros(time), time);
             assertTrue(time > last);
             last = time;
         }
@@ -23,12 +25,13 @@ public class UniqueMicroTimeProviderTest {
     @Test
     public void currentTimeNanos() {
         UniqueMicroTimeProvider tp = new UniqueMicroTimeProvider();
-        SetTimeProvider stp = new SetTimeProvider();
+        SetTimeProvider stp = new SetTimeProvider(SystemTimeProvider.INSTANCE.currentTimeNanos());
         tp.provider(stp);
         long last = 0;
         for (int i = 0; i < 4_000; i++) {
             stp.advanceNanos(i);
             long time = tp.currentTimeNanos();
+            assertEquals(LongTime.toNanos(time), time);
             assertTrue(time / 1000 > last);
             last = time / 1000;
         }
