942213f21193f5296f71589799994f50aff45beb
Peter Lawrey <peter.lawrey@chronicle.software>
2021-06-18 14:35:39 +0100

    Handle slow cleanup of files.

--- src/test/java/net/openhft/chronicle/core/CleaningRandomAccessFileTest.java
+++ src/test/java/net/openhft/chronicle/core/CleaningRandomAccessFileTest.java
@@ -33,11 +33,12 @@ public class CleaningRandomAccessFileTest {
     public void resourceLeak() throws IOException {
         File tempDir = IOTools.createTempFile("resourceLeak");
         tempDir.mkdir();
-        for (int j = 0; j < 50; j++) {
+        int repeat = Jvm.isArm() ? 10 : 50;
+        for (int j = 0; j < repeat; j++) {
             int files = getFDs();
             if (files > 0) {
 //                System.out.println("File descriptors " + files);
-                assertEquals(200, files, 200);
+                assertEquals("j: " + j, 200, files, 200);
             }
             ByteBuffer bb = ByteBuffer.allocateDirect(64);
             for (int i = 0; i < 200; i++) {
@@ -45,8 +46,18 @@ public class CleaningRandomAccessFileTest {
                 bb.clear();
                 file.getChannel().write(bb);
             }
+            long start = System.currentTimeMillis();
             System.gc();
-            Jvm.pause(Jvm.isAzulZing() ? 100 : 10);
+            for (int i = 0; i < 40; i++) {
+                Jvm.pause(20);
+                if (getFDs() < 400) {
+                    double time = (System.currentTimeMillis() - start) / 1e3;
+                    if (time > 0.1)
+                        System.out.println("resourceLeak() - Took " + time + " seconds.");
+                    break;
+                }
+            }
         }
+        IOTools.deleteDirWithFiles(tempDir);
     }
 }
