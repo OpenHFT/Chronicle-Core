2d1a2d3567d84893066c757217e8c23077d8b65b
Jerry Shea <jerry.shea@chronicle.software>
2021-08-09 11:38:24 +1000

    de-flake StackTraceTest #242

--- src/test/java/net/openhft/chronicle/core/StackTraceTest.java
+++ src/test/java/net/openhft/chronicle/core/StackTraceTest.java
@@ -2,18 +2,29 @@ package net.openhft.chronicle.core;
 
 import org.junit.Test;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class StackTraceTest {
+    private static final CountDownLatch threadStarted = new CountDownLatch(1);
 
     static void thinking() {
-        Jvm.pause(1000);
+        threadStarted.countDown();
+        Jvm.pause(5_000);
     }
 
     @Test
-    public void forThread() {
+    public void forThread() throws InterruptedException {
+        // load Jvm class before
+        Jvm.init();
         Thread t = new Thread(StackTraceTest::thinking, "background");
         t.start();
+        boolean started = threadStarted.await(1, TimeUnit.SECONDS);
+        assertTrue(started);
+        // wait for Jvm.pause to start
         Jvm.pause(50);
         StackTrace st = StackTrace.forThread(t);
         t.interrupt();
