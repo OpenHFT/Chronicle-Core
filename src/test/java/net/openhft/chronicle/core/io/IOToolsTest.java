01d3fb09a54db9027b21f4b7e4440073e6e77424
Jerry Shea <jerry.shea@chronicle.software>
2021-10-11 15:38:49 +1100

    Java 11 build - tidy

--- src/test/java/net/openhft/chronicle/core/io/IOToolsTest.java
+++ src/test/java/net/openhft/chronicle/core/io/IOToolsTest.java
@@ -52,8 +52,6 @@ public class IOToolsTest extends CoreTestCommon {
 
     @Test
     public void shouldCleanDirectBuffer() {
-        if (Jvm.isJava9Plus())
-            expectException("Unable to find suitable cleaner service, falling back to using reflection");
         IOTools.clean(ByteBuffer.allocateDirect(64));
     }
 
