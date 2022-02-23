30c5dfe8af7a1e5d12f15c5fe19c8132ee013ab7
Peter Lawrey <peter.lawrey@chronicle.software>
2022-02-23 16:41:51 +0000

    SystemTimeProvider drops uniqueness across multiples of 32 ns #349

--- src/main/java/net/openhft/chronicle/core/time/SystemTimeProvider.java
+++ src/main/java/net/openhft/chronicle/core/time/SystemTimeProvider.java
@@ -19,29 +19,24 @@
 package net.openhft.chronicle.core.time;
 
 import net.openhft.chronicle.core.Jvm;
-import net.openhft.chronicle.core.UnsafeMemory;
-import net.openhft.chronicle.core.annotation.UsedViaReflection;
 
 public enum SystemTimeProvider implements TimeProvider {
     INSTANCE;
 
-    private static final long LAST_NANOS;
+    private static final int NANOS_PER_MILLI = 1_000_000;
     // Can be overridden for testing purposes.
     public static TimeProvider CLOCK = INSTANCE;
 
     static {
         // warmUp()
         long start = System.currentTimeMillis();
-        while (System.currentTimeMillis() < start + 3) {
-            INSTANCE.currentTimeNanos1();
+        while (System.currentTimeMillis() < start + 5) {
+            INSTANCE.currentTimeNanos();
             Jvm.nanoPause();
         }
-        LAST_NANOS = UnsafeMemory.unsafeObjectFieldOffset(Jvm.getField(SystemTimeProvider.class, "lastNanos5"));
     }
 
     private long delta = 0;
-    @UsedViaReflection
-    private volatile long lastNanos5;
 
     @Override
     public long currentTimeMillis() {
@@ -54,34 +49,20 @@ public enum SystemTimeProvider implements TimeProvider {
     }
 
     /**
-     * @return a nano second time stamp which is monotonically increasing and the top 59 bits are unique. i.e. 32 ns resolution.
+     * @return a nanosecond time stamp which is generally monotonically increasing
      */
     @Override
     public long currentTimeNanos() {
-        long timeNanos = currentTimeNanos1();
-        while (true) {
-            long last5 = this.lastNanos5;
-            long timeNanos5 = timeNanos >>> 5;
-            if (timeNanos5 <= last5) {
-                timeNanos5 = last5 + 1;
-                timeNanos = timeNanos5 << 5;
-            }
-            if (UnsafeMemory.INSTANCE.compareAndSwapLong(this, LAST_NANOS, last5, timeNanos5))
-                return timeNanos;
-        }
-    }
-
-    protected long currentTimeNanos1() {
         long nowNS = System.nanoTime();
-        long nowMS = currentTimeMillis() * 1000000;
+        long nowMS = currentTimeMillis() * NANOS_PER_MILLI;
         long estimate = nowNS + delta;
 
         if (estimate < nowMS) {
             delta = nowMS - nowNS;
             return nowMS;
 
-        } else if (estimate > nowMS + 1000000) {
-            nowMS += 1000000;
+        } else if (estimate > nowMS + NANOS_PER_MILLI) {
+            nowMS += NANOS_PER_MILLI;
             delta = nowMS - nowNS;
             return nowMS;
         }
