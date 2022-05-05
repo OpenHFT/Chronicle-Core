446cad6cd6174e6b41d6f6e94c7f64b106577e64
Per-Åke Minborg <minborg@speedment.com>
2022-05-05 14:05:22 +0200

    Do not merge: Discussion about "domestic" use (#389)
    * Add example of domestic use
    
    * Cleanup PR before merge

---
+++ src/main/java/net/openhft/chronicle/core/domestic/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * This package and any and all sub-packages contains strictly internal classes reserved for Chronicle Software.
+ * Domestic classes shall <em>never</em> be used directly by end-user code. Instead, they are only available
+ * for use internally by other Chronicle libraries.
+ * <p>
+ * Specifically, the following actions (including, but not limited to) are not allowed
+ * on internal classes and packages:
+ *  <ul>
+ *      <li>Casting to</li>
+ *      <li>Reflection of any kind</li>
+ *      <li>Explicit Serialize/deserialize</li>
+ *  </ul>
+ * <p>
+ * The classes in this package and any sub-package are subject to change at any time for any reason.
+ */
+package net.openhft.chronicle.core.domestic;
