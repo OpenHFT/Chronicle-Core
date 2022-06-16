58f79cb6c5c95b97c062426d215a42d3552eb6c7
Nick Tindall <nick@tindall.id.au>
2022-06-16 14:42:29 +1000

    Expand applicability of @RequiredForClient

--- src/main/java/net/openhft/chronicle/core/annotation/RequiredForClient.java
+++ src/main/java/net/openhft/chronicle/core/annotation/RequiredForClient.java
@@ -17,16 +17,13 @@
  */
 package net.openhft.chronicle.core.annotation;
 
-import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
 
 /**
  * Be careful how this class is modified as it is referred to by name in a contract.
  */
 @Retention(RetentionPolicy.SOURCE)
-@Target(ElementType.TYPE)
 public @interface RequiredForClient {
     String value() default "";
 }
