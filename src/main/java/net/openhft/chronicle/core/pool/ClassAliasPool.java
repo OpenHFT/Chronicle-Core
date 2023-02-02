OpenHFT/Chronicle-Core

commit df446c471e817f1c3a70bed020601b774245d8bd

Don't use the simpleName ad a class alias if the name has been given.

src/main/java/net/openhft/chronicle/core/pool/ClassAliasPool.java
@@ -224,7 +224,8 @@ public class ClassAliasPool implements ClassLookup {
             warnIfChanged(prev, clazz, "Replaced");
             stringClassMap2.putIfAbsent(new CAPKey(toCamelCase(name)), clazz);
             classStringMap.putIfAbsent(clazz, name);
-            addAlias(clazz);
+            Class<?> prev1 = stringClassMap.putIfAbsent(new CAPKey(clazz.getName()), clazz);
+            warnIfChanged(prev1, clazz, "Did not replace by name");
         }
     }
 

commit d29189d1eda1272c3bb510652e3ef6f10dc61fff

Add back in specific simple name class aliases (fix breaks caused by https://github.com/OpenHFT/Chronicle-Core/commit/df446c471e817f1c3a70bed020601b774245d8bd)

src/main/java/net/openhft/chronicle/core/pool/ClassAliasPool.java
@@ -72,14 +72,14 @@ public class ClassAliasPool implements ClassLookup {
         addAlias(CharSequence.class);
         addAlias(Byte.class, "byte, int8");
         addAlias(Short.class, "short, int16");
-        addAlias(Character.class, "Char");
-        addAlias(Integer.class, "int, int32");
+        addAlias(Character.class, "Char, Character");
+        addAlias(Integer.class, "int, int32, Integer");
         addAlias(Long.class, "long, int64");
-        addAlias(Float.class, "Float32");
-        addAlias(Double.class, "Float64");
-        addAlias(LocalDate.class, "Date");
-        addAlias(LocalDateTime.class, "DateTime");
-        addAlias(LocalTime.class, "Time");
+        addAlias(Float.class, "Float32, Float");
+        addAlias(Double.class, "Float64, Double");
+        addAlias(LocalDate.class, "Date, LocalDate");
+        addAlias(LocalDateTime.class, "DateTime, LocalDateTime");
+        addAlias(LocalTime.class, "Time, LocalTime");
         addAlias(ZonedDateTime.class, "ZonedDateTime");
         addAlias(TimeUnit.class, "TimeUnit");
         addAlias(String[].class, "String[]");