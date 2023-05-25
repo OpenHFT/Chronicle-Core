66d1a18f0f3bf03c5a5a150795f44b06780f2ddb
Gonzalo Ortiz <gortiz@users.noreply.github.com>
2023-05-25 12:37:24 +0200

    OS.mapAlign is not working correctly when offset is not already aligned.

--- src/main/java/net/openhft/chronicle/core/OS.java
+++ src/main/java/net/openhft/chronicle/core/OS.java
@@ -285,7 +285,11 @@ public final class OS {
      */
     public static long mapAlign(long offset) {
         int chunkMultiple = (int) mapAlignment();
-        return (offset + chunkMultiple - 1) / chunkMultiple * chunkMultiple;
+        return mapAlign(offset, chunkMultiple);
+    }
+
+    public static long mapAlign(long offset, int pageAlignment) {
+        return ((offset + pageAlignment) / pageAlignment - 1) * pageAlignment;
     }
 
     /**
