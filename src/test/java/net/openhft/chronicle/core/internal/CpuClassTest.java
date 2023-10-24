82ac75a09ed81b5e97b66ee4f46cdc0334bd1368
Peter Lawrey <peter.lawrey@chronicle.software>
2023-10-24 15:27:57 +0100

    CpuClass is now a top level class in internal

---
+++ src/test/java/net/openhft/chronicle/core/internal/CpuClassTest.java
@@ -0,0 +1,36 @@
+package net.openhft.chronicle.core.internal;
+
+import net.openhft.chronicle.core.Jvm;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.junit.Assume.assumeFalse;
+
+public class CpuClassTest {
+    @Test
+    public void getCpuModel() {
+        final String cpuClass = CpuClass.getCpuModel();
+        System.out.println("cpuClass: " + cpuClass + ", os.name: " + System.getProperty("os.name") + ", os.arch: " + System.getProperty("os.arch"));
+        if (Jvm.isMacArm()) {
+            assertEquals(cpuClass, "Apple M1", cpuClass);
+
+        } else if (Jvm.isArm()) {
+            assertTrue(cpuClass, cpuClass.startsWith("ARMv"));
+
+        } else {
+            assertTrue(cpuClass, (cpuClass.contains("Intel") && cpuClass.contains(" @ "))
+                    || (cpuClass.startsWith("AMD ")));
+        }
+
+        assertNotNull(cpuClass);
+    }
+
+    @Test
+    public void removingTag() {
+        // TODO FIX on MacOS. sysctl -a returned 141, https://github.com/OpenHFT/Chronicle-Core/issues/557
+        assumeFalse(net.openhft.chronicle.core.internal.Bootstrap.IS_MAC);
+        final String actual = CpuClass.removingTag().apply("tag: value");
+        assertEquals("value", actual);
+    }
+
+}
