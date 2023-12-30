1bf8bf871df52dfcdb24b7289f4453e05715d6b5
Peter Lawrey <peter.lawrey@chronicle.software>
2023-12-30 15:30:03 +0000

    Fix package

--- src/test/java/net/openhft/chronicle/core/scoped/ScopedThreadLocalTest.java
+++ src/test/java/net/openhft/chronicle/core/scoped/ScopedThreadLocalTest.java
@@ -2,8 +2,6 @@ package net.openhft.chronicle.core.scoped;
 
 import net.openhft.chronicle.core.CoreTestCommon;
 import net.openhft.chronicle.core.Jvm;
-import net.openhft.chronicle.core.scoped.ScopedResource;
-import net.openhft.chronicle.core.scoped.ScopedThreadLocal;
 import net.openhft.chronicle.core.threads.CleaningThread;
 import org.junit.Before;
 import org.junit.Test;
