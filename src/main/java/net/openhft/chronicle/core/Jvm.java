515eee2d0e6930ec463579be7e6463bfa32b6f28
yevgen.pavlenko <yevgen.pavlenko@chronicle.software>
2024-04-03 13:30:31 +0300

    Revert changes to Jvm.parseSize

--- src/main/java/net/openhft/chronicle/core/Jvm.java
+++ src/main/java/net/openhft/chronicle/core/Jvm.java
@@ -1223,7 +1223,7 @@ public final class Jvm {
             }
         }
         double number = Double.parseDouble(value.trim());
-        factor *= (long) number;
+        factor *= number;
         return factor;
     }
 
