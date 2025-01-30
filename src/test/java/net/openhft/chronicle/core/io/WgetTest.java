c11d5045e1c6c8d330763339ce672e6555d21cb7
jamesmcsherry <james.mcsherry@chronicle.software>
2025-01-30 13:41:09 +0000

    Temporary disable testHttpGetRequest

--- src/test/java/net/openhft/chronicle/core/io/WgetTest.java
+++ src/test/java/net/openhft/chronicle/core/io/WgetTest.java
@@ -1,5 +1,6 @@
 package net.openhft.chronicle.core.io;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -7,8 +8,9 @@ import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class WgetTest {
+class WgetTest {
 
+    @Disabled("Temporarily disabled")
     @Test
     public void testHttpGetRequest() throws IOException {
         String testUrl = "http://example.com";
