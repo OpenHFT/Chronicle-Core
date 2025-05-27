691e366bb4a0b7687c7d0fd7ffec28383e7ba408
Peter Lawrey <peter.lawrey@chronicle.software>
2025-05-27 16:21:56 +0100

    Document strong vs weak scoped resources

--- src/main/java/net/openhft/chronicle/core/scoped/StrongReferenceScopedResource.java
+++ src/main/java/net/openhft/chronicle/core/scoped/StrongReferenceScopedResource.java
@@ -18,10 +18,13 @@ package net.openhft.chronicle.core.scoped;
 import static net.openhft.chronicle.core.io.Closeable.closeQuietly;
 
 /**
- * A {@link ScopedResource} that will always retain a strong reference to the
- * contained resource, even when not "in use"
+ * A {@link ScopedResource} that always keeps a strong reference to its
+ * resource. The object therefore remains reachable until
+ * {@link #closeResource()} is called. In contrast to
+ * {@link WeakReferenceScopedResource}, the reference is never cleared when the
+ * scope ends.
  *
- * @param <T> The type of the contained resource
+ * @param <T> the type of the contained resource
  */
 public class StrongReferenceScopedResource<T> extends AbstractScopedResource<T> {
 
