f22f3b41240390aafe125c511bc970e27f85132e
Peter Lawrey <peter.lawrey@chronicle.software>
2025-05-27 16:24:04 +0100

    Add class javadoc and null contract (#771)

--- src/main/java/net/openhft/chronicle/core/pom/PomProperties.java
+++ src/main/java/net/openhft/chronicle/core/pom/PomProperties.java
@@ -21,6 +21,22 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Properties;
 
+/**
+ * Access to Maven build metadata at runtime.
+ *
+ * <p>It loads the {@code pom.properties} resource for a given
+ * {@code groupId} and {@code artifactId}, allowing callers to
+ * inspect version and other build-time information.</p>
+ *
+ * <p>All methods require non-null parameters. Passing {@code null}
+ * results in a {@link NullPointerException}.</p>
+ *
+ * <p>Example:</p>
+ * <pre>{@code
+ * Properties props = PomProperties.create("net.openhft", "chronicle-core");
+ * String version = props.getProperty("version");
+ * }</pre>
+ */
 public final class PomProperties {
 
     private PomProperties() {
@@ -39,7 +55,7 @@ public final class PomProperties {
      * @return a new instance of Properties for the provided parameters.
      */
     @NotNull
-    public static Properties create(final String groupId, final String artifactId) {
+    public static Properties create(@NotNull final String groupId, @NotNull final String artifactId) {
         return InternalPomProperties.create(groupId, artifactId);
     }
 
@@ -56,7 +72,7 @@ public final class PomProperties {
      * the version cannot be determined
      * @see #create(String, String)
      */
-    public static String version(final String groupId, final String artifactId) {
+    public static String version(@NotNull final String groupId, @NotNull final String artifactId) {
         return InternalPomProperties.version(groupId, artifactId);
     }
 }
