708155ab37307aa8da18dad5a6a9a4ce8099317f
Peter Lawrey <peter.lawrey@chronicle.software>
2025-05-29 13:51:39 +0100

    Update copyright years to 2025 in multiple files

--- benchmarks/src/main/java/net/openhft/chronicle/core/benchmark/Main.java
+++ benchmarks/src/main/java/net/openhft/chronicle/core/benchmark/Main.java
@@ -1,5 +1,5 @@
 /*
- *     Copyright (C) 2015-2020 chronicle.software
+ *     Copyright 2015-2025 chronicle.software
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU Lesser General Public License as published by
