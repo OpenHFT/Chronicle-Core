a6d409eca9d5f2ab5500cab15aa45574db3203d7
Peter Lawrey <peter.lawrey@chronicle.software>
2025-06-03 13:46:43 +0100

    Refactor test imports and remove unused methods in CleaningRandomAccessFileTest

--- src/test/java/net/openhft/chronicle/core/io/CleaningRandomAccessFileTest.java
+++ src/test/java/net/openhft/chronicle/core/io/CleaningRandomAccessFileTest.java
@@ -3,7 +3,7 @@ package net.openhft.chronicle.core.io;
 import net.openhft.chronicle.core.CoreTestCommon;
 import net.openhft.chronicle.core.Jvm;
 import net.openhft.chronicle.core.OS;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,7 +12,8 @@ import java.nio.ByteBuffer;
 import java.nio.file.Files;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CleaningRandomAccessFileTest extends CoreTestCommon {
 
@@ -37,6 +38,7 @@ public class CleaningRandomAccessFileTest extends CoreTestCommon {
     public void testFinalizeAndCleanup() throws IOException {
         File tempFile = File.createTempFile("test", "raf");
 
+        //noinspection resource
         new CleaningRandomAccessFile(tempFile, "rw");
 
         System.gc();
@@ -48,15 +50,10 @@ public class CleaningRandomAccessFileTest extends CoreTestCommon {
         }
     }
 
-    static int getFDs() {
-        if (!OS.isLinux())
-            return -1;
-        return new File("/proc/self/fd").list().length;
-    }
-
     @Test
     public void resourceLeak() throws IOException {
         File tempDir = IOTools.createTempFile("resourceLeak");
+        //noinspection ResultOfMethodCallIgnored
         tempDir.mkdir();
         int repeat = Jvm.isArm() ? 6 : OS.isWindows() ? 25 : 50;
         for (int j = 0; j < repeat; j++) {
@@ -67,8 +64,10 @@ public class CleaningRandomAccessFileTest extends CoreTestCommon {
             }
             ByteBuffer bb = ByteBuffer.allocateDirect(64);
             for (int i = 0; i < 200; i++) {
+                @SuppressWarnings("resource")
                 RandomAccessFile file = new CleaningRandomAccessFile(tempDir + "/file" + i, "rw");
                 bb.clear();
+                //noinspection ResultOfMethodCallIgnored
                 file.getChannel().write(bb);
             }
             long start = System.currentTimeMillis();
@@ -89,38 +88,7 @@ public class CleaningRandomAccessFileTest extends CoreTestCommon {
     static int getFDs() {
         if (!OS.isLinux())
             return -1;
+        //noinspection DataFlowIssue
         return new File("/proc/self/fd").list().length;
-    }
-
-    @org.junit.Test
-    public void resourceLeak() throws IOException {
-        File tempDir = IOTools.createTempFile("resourceLeak");
-        tempDir.mkdir();
-        int repeat = Jvm.isArm() ? 6 : OS.isWindows() ? 25 : 50;
-        for (int j = 0; j < repeat; j++) {
-            int files = getFDs();
-            if (files > 0) {
-//                System.out.println("File descriptors " + files);
-                assertEquals("j: " + j, 200, files, 200);
-            }
-            ByteBuffer bb = ByteBuffer.allocateDirect(64);
-            for (int i = 0; i < 200; i++) {
-                RandomAccessFile file = new CleaningRandomAccessFile(tempDir + "/file" + i, "rw");
-                bb.clear();
-                file.getChannel().write(bb);
-            }
-            long start = System.currentTimeMillis();
-            System.gc();
-            for (int i = 0; i < 40; i++) {
-                Jvm.pause(20);
-                if (getFDs() < 200) {
-                    double time = (System.currentTimeMillis() - start) / 1e3;
-                    if (time > 0.1)
-                        System.out.println("resourceLeak() - Took " + time + " seconds.");
-                    break;
-                }
-            }
-        }
-        IOTools.deleteDirWithFiles(tempDir);
     }
 }
