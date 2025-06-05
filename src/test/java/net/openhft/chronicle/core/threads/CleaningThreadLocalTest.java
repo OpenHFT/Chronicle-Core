aad2b66209c24d16a108119c7038c5549b84ed18
Peter Lawrey <peter.lawrey@chronicle.software>
2025-06-05 10:44:15 +0100

    Refactor CleaningThreadLocalTest to use withCleanup method and remove disabled test

--- src/test/java/net/openhft/chronicle/core/threads/CleaningThreadLocalTest.java
+++ src/test/java/net/openhft/chronicle/core/threads/CleaningThreadLocalTest.java
@@ -1,11 +1,14 @@
 package net.openhft.chronicle.core.threads;
 
-import static org.mockito.Mockito.*;
-import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import java.util.function.Supplier;
 import net.openhft.chronicle.core.util.ThrowingConsumer;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Supplier;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class CleaningThreadLocalTest {
 
@@ -14,7 +17,7 @@ public class CleaningThreadLocalTest {
         Supplier<String> supplier = () -> "test";
         ThrowingConsumer<String, Exception> cleanup = value -> { /* cleanup logic */ };
 
-        CleaningThreadLocal<String> ctl = new CleaningThreadLocal<>(supplier, cleanup);
+        CleaningThreadLocal<String> ctl = CleaningThreadLocal.withCleanup(supplier, cleanup);
 
         assertNotNull(ctl);
     }
@@ -43,34 +46,10 @@ public class CleaningThreadLocalTest {
     }
 
     @Test
-    public void testResourceCleanup_disableViaSystemProperty() {
-        try {
-
-            System.setProperty(CleaningThreadLocal.CHRONICLE_TRACK_NON_CLEANING_THREADS_ENABLED, "false");
-
-            Runnable cleanupAction = mock(Runnable.class);
-            CleaningThreadLocal<Runnable> ctl = CleaningThreadLocal.withCleanup(() -> cleanupAction, Runnable::run);
-
-            Thread thread = new Thread(() -> {
-                ctl.set(cleanupAction);
-                ctl.remove();
-            });
-            thread.start();
-            joinThread(thread);
-
-            // Clean up action *should not* be run as it has been disabled
-            verify(cleanupAction, times(0)).run();
-
-        } finally {
-            System.clearProperty(CleaningThreadLocal.CHRONICLE_TRACK_NON_CLEANING_THREADS_ENABLED);
-        }
-    }
-
-    @Test
     public void testThreadSafety() throws InterruptedException {
         Supplier<String> supplier = () -> "test";
         ThrowingConsumer<String, Exception> cleanup = value -> { /* cleanup logic */ };
-        CleaningThreadLocal<String> ctl = new CleaningThreadLocal<>(supplier, cleanup);
+        CleaningThreadLocal<String> ctl = CleaningThreadLocal.withCleanup(supplier, cleanup);
 
         Thread t1 = new Thread(() -> ctl.set("Thread 1"));
         Thread t2 = new Thread(() -> ctl.set("Thread 2"));
@@ -88,7 +67,7 @@ public class CleaningThreadLocalTest {
         ThrowingConsumer<String, Exception> cleanup = value -> {
             throw new RuntimeException("Cleanup failed");
         };
-        CleaningThreadLocal<String> ctl = new CleaningThreadLocal<>(supplier, cleanup);
+        CleaningThreadLocal<String> ctl = CleaningThreadLocal.withCleanup(supplier, cleanup);
     }
 
     @Test
