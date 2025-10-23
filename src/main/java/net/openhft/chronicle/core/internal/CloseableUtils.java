2129c90e324667f7ee2d1506e388e69ae8c868c2
Peter Lawrey <peter.lawrey@chronicle.software>
2025-10-23 15:30:56 +0100

    Make closeQuietly method public for direct call

--- src/main/java/net/openhft/chronicle/core/internal/CloseableUtils.java
+++ src/main/java/net/openhft/chronicle/core/internal/CloseableUtils.java
@@ -311,7 +311,7 @@ public final class CloseableUtils {
      *
      * @param o the object to close
      */
-    static void closeQuietly(@Nullable Object o) {
+    public static void closeQuietly(@Nullable Object o) {
         if (o instanceof Collection) {
             Collection<?> coll = (Collection<?>) o;
             if (coll.isEmpty())
