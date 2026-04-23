9da536051e9e7c65ffef48847ff2f2f12d2f7a43
Peter Lawrey <peter.lawrey@chronicle.software>
2026-04-23 11:07:25 +0100

    Run cleaner reflection test on Java 21

--- src/test/java/net/openhft/chronicle/core/internal/cleaner/ReflectionBasedByteBufferCleanerServiceTest.java
+++ src/test/java/net/openhft/chronicle/core/internal/cleaner/ReflectionBasedByteBufferCleanerServiceTest.java
@@ -6,6 +6,7 @@ package net.openhft.chronicle.core.internal.cleaner;
 import net.openhft.chronicle.core.internal.cleaner.ReflectionBasedByteBufferCleanerService;
 import net.openhft.chronicle.core.cleaner.spi.ByteBufferCleanerService.Impact;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 
 import java.nio.ByteBuffer;
@@ -24,7 +25,8 @@ class ReflectionBasedByteBufferCleanerServiceTest {
     }
 
     @Test
-    @EnabledIfSystemProperty(named = "java.version", matches = "9|1[0-9].*")
+    // Match anything that is not a legacy "1.x" version string -- covers Java 9+ regardless of major version.
+    @DisabledIfSystemProperty(named = "java.version", matches = "1\\..*")
     void cleanShouldWorkOnJava9Plus() {
         ByteBuffer buffer = ByteBuffer.allocateDirect(1024);
         assertDoesNotThrow(() -> cleanerService.clean(buffer), "Cleaning a direct buffer should not throw an exception on Java 9+");
