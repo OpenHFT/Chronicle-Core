35e1b3c5519e17fa9a47bd7ca0e0c20ff8634caf
Peter Lawrey <peter.lawrey@chronicle.software>
2026-04-23 11:07:16 +0100

    Cover ExceptionHandler default helpers

---
+++ src/test/java/net/openhft/chronicle/core/onoes/ExceptionHandlerTest.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2013-2025 chronicle.software; SPDX-License-Identifier: Apache-2.0
+ */
+package net.openhft.chronicle.core.onoes;
+
+import net.openhft.chronicle.core.util.IgnoresEverything;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExceptionHandlerTest {
+
+    @Test
+    void ignoresEverythingReturnsNullHandler() {
+        assertSame(NullExceptionHandler.NOTHING, ExceptionHandler.ignoresEverything());
+        assertInstanceOf(IgnoresEverything.class, ExceptionHandler.ignoresEverything());
+    }
+
+    @Test
+    void nullExceptionHandlerUsesInheritedClassBasedDefault() {
+        assertDoesNotThrow(() ->
+                NullExceptionHandler.NOTHING.on(ExceptionHandlerTest.class, "ignored", new RuntimeException("ignored")));
+    }
+}
